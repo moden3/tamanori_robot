ac1109367032aa541c975181276275a6c3b28701
m-sato-1998 <m.satoh.1998@gmail.com>
2019-08-28 17:51:07 +0900

    08281751

--- source/tamanori_main/src/motor_pid_controll.cpp
+++ source/tamanori_main/src/motor_pid_controll.cpp
@@ -28,7 +28,83 @@ class PID
         double lateD;
 };
 
-void pid_controll(){
+PID::PID()
+{
+    integral = 0;
+    prev_hensa = 0;
+    nowtime = 0;
+    prev_time = 0;
+    lateD = 0;
+}
+
+double PID::control(double target, double nowrpm)
+{
+    nowtime = ;
+    double hensa = target - nowrpm;
+    double dt = ;
+    integral += (hensa + prev_hensa) / 2 * dt;
+    double differential = (hensa - prev_hensa) / dt;
+    double sousaryou = kp*hensa +ki*integral + kd*differential;
+    prev_hensa = hensa;
+    prev_time = ;
+    return sousaryou;    
+}
+
+double PID::PI_lateD(double target, double nowrpm)
+{
+    nowtime = micros();
+    double hensa = target - nowrpm;
+    double dt = nowtime - prev_time;
+    integral += (hensa + prev_hensa) / 2 * dt;
+    double sousaryou = kp*hensa + ki*integral + kd*lateD;
+    prev_hensa = hensa;
+    prev_time = micros();
+    lateD = (hensa - prev_hensa) / dt;
+    return sousaryou;
+}
+
+double PID::control_P(double target, double nowrpm, double new_kp)
+{
+    double hensa = target - nowrpm;
+    double sousaryou = new_kp*hensa;
+    return sousaryou;
+}
+
+double PID::control_PI(double target, double nowrpm)
+{
+    kp = 0.45 * ku;
+    ti = 0.83 * pu;
+    ki = (1 / ti) * kp;
+    nowtime = ;
+    double hensa = target - nowrpm;
+    double dt = nowtime - prev_time;
+    integral += (hensa + prev_hensa) / 2 * dt;
+    double sousaryou = kp*hensa + ki*integral;
+    prev_hensa = hensa;
+    prev_time = ;
+    return sousaryou;
+}
+void PID::setParameter(double new_kp, double new_ki, double new_kd)
+{  
+  kp = new_kp;
+  ki = new_ki;
+  kd = new_kd;
+}
+void PID::setParameter(double new_ku, double new_pu)
+{  
+    ku = new_ku;
+    pu = new_pu;
     
+    kp = 0.60 * ku;
+    ti = 0.50 * pu;
+    td = 0.125 * pu;
+    ki = (1 / ti) * kp;
+    kd = td * kp;
+}
+void PID::reset(double target)
+{
+    integral    = 0;
+    prev_hensa  = target;
+    prev_time = ;
 }
 

0e31537da7cfba56738b596dcd4bf6b7e8d7d71c
moden3 <akrmts720@gmail.com>
2019-11-26 09:26:10 +0900

    201911260925 bno_test

--- source/tamanori_main/src/anglecontrol.cpp
+++ source/tamanori_main/src/anglecontrol.cpp
@@ -5,10 +5,10 @@
 void set_spin(){
     double K_theta = 20.0;   // 角度の比例ゲイン
     double K_theta_I = 0.1;
-    double K_omega = 0.0;     // 角速度の比例ゲイン
+    double K_omega = 0.0;    // 角速度の比例ゲイン
 
     static double theta_sum = 0.0;
     for (int i = 0; i < 3;i++){
-        omegavec[i] = rotvector[i] * (rotvector[3] * K_theta+theta_sum*); // + rotomega_stock * K_omega);
+        omegavec[i] = rotvector[i] * (rotvector[3] * K_theta+theta_sum); // + rotomega_stock * K_omega);
     }
 }
